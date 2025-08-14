4ca91462799d8e30853204d23ac271ce7937346e
daseul park <seuli0502@gmail.com>
2025-08-14 09:39:21 +0900

    feat: Comment Routers 및 Comment Service 개발

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import styleRouter from './Routers/Style.js';
 import { CurationRouter } from './Routers/Curation.js';
+import { commentRouter } from './Routers/Commetnts.js';
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/styles', styleRouter);
 app.use('/curations', CurationRouter);
+app.use('/comments', commentRouter);
 
 app.listen(PORT, () => {
   console.log(`서버가 ${PORT}에서 실행중입니다.`);
