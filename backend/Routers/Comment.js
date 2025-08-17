64e81071863ef601ed1d77fa2541621ad228d315
daseul park <seuli0502@gmail.com>
2025-08-18 00:20:48 +0900

    refactor: Comment파일 중복코드 정리

--- backend/Routers/Comment.js
+++ backend/Routers/Comment.js
@@ -2,18 +2,16 @@ import express from 'express';
 // prettier-ignore
 import { postComment, putComment, deleteComment } from '../Services/CommentService.js';
 // prettier-ignore
-import { validateCommentId, validateCreateComment, validateUpdateComment, validateCurationId } from '../Validators/CommentValidator.js';
+import { validatePostComment, validatePutComment, validateDeleteComment } from '../Validators/CommentValidator.js';
 
 const curationNestedCommentRouter = express.Router({ mergeParams: true });
 const commentRouter = express.Router();
 
-// prettier-ignore
-curationNestedCommentRouter.route('/')
-  .post(validateCurationId, validateCreateComment, postComment());
+curationNestedCommentRouter.route('/').post(validatePostComment, postComment());
 
-// prettier-ignore
-commentRouter.route('/:id')
-  .put(validateCommentId, validateUpdateComment, putComment())
-  .delete(validateCommentId, deleteComment());
+commentRouter
+  .route('/:id')
+  .put(validatePutComment, putComment())
+  .delete(validateDeleteComment, deleteComment());
 
 export { curationNestedCommentRouter, commentRouter };
