83d14a299b1d9624b405f6a95fcf3f4e758cfd80
오연진 <yonjin.oh@gmail.com>
2025-08-25 12:08:05 +0900

    feat: 스타일 url 복사하기 버튼 추가

--- frontend/src/libs/shared/layout/OptionButtonsLayout.tsx
+++ frontend/src/libs/shared/layout/OptionButtonsLayout.tsx
@@ -1,22 +1,36 @@
-'use client'
+"use client";
 
-import classNames from 'classnames/bind'
-import styles from './OptionButtonsLayout.module.scss'
+import classNames from "classnames/bind";
+import styles from "./OptionButtonsLayout.module.scss";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 type OptionButtonsLayoutProps = {
-  onClickEdit: () => void
-  onClickDelete: () => void
-}
+  onClickEdit: () => void;
+  onClickDelete: () => void;
+  onClickCopy?: () => void;
+};
 
-const OptionButtonsLayout = ({ onClickEdit, onClickDelete }: OptionButtonsLayoutProps) => {
+const OptionButtonsLayout = ({
+  onClickEdit,
+  onClickDelete,
+  onClickCopy,
+}: OptionButtonsLayoutProps) => {
   return (
-    <div className={cx('container')}>
-      <button onClick={onClickEdit} className={cx('button')}>수정하기</button>
-      <button onClick={onClickDelete} className={cx('button')}>삭제하기</button>
+    <div className={cx("container")}>
+      {onClickCopy && (
+        <button onClick={onClickCopy} className={cx("button")}>
+          복사하기
+        </button>
+      )}
+      <button onClick={onClickEdit} className={cx("button")}>
+        수정하기
+      </button>
+      <button onClick={onClickDelete} className={cx("button")}>
+        삭제하기
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default OptionButtonsLayout
+export default OptionButtonsLayout;
--- frontend/src/libs/style-detail/feature-style-detail/StyleOptionButtons.tsx
+++ frontend/src/libs/style-detail/feature-style-detail/StyleOptionButtons.tsx
@@ -1,65 +1,83 @@
-'use client'
+"use client";
 
-import OptionButtonsLayout from '@libs/shared/layout/OptionButtonsLayout'
-import FormModal from '@libs/shared/modal/form-modal/FormModal'
-import useModal from '@libs/shared/modal/useModal'
-import { useRouter } from 'next/navigation'
-import StyleDeleteForm from './StyleDeleteForm'
-import useConfirmModal from '@libs/shared/modal/useConfirmModal'
-import deleteStyle from '../data-access-style-detail/deleteStyle'
-import { StyleDeleteFormInput } from '@services/types'
+import OptionButtonsLayout from "@libs/shared/layout/OptionButtonsLayout";
+import FormModal from "@libs/shared/modal/form-modal/FormModal";
+import useModal from "@libs/shared/modal/useModal";
+import { useRouter } from "next/navigation";
+import StyleDeleteForm from "./StyleDeleteForm";
+import useConfirmModal from "@libs/shared/modal/useConfirmModal";
+import deleteStyle from "../data-access-style-detail/deleteStyle";
+import { StyleDeleteFormInput } from "@services/types";
 
 type StyleOptionButtonsProps = {
-  styleId: number
-}
+  styleId: number;
+};
 
 const StyleOptionButtons = ({ styleId }: StyleOptionButtonsProps) => {
-  const styleDeleteFormModal = useModal()
-  const { renderConfirmModal, openConfirmModal } = useConfirmModal()
-  const router = useRouter()
+  const styleDeleteFormModal = useModal();
+  const { renderConfirmModal, openConfirmModal } = useConfirmModal();
+  const router = useRouter();
 
+  const handleCopyUrl = async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      openConfirmModal({
+        description: "클립보드에 복사되었습니다.",
+      });
+    } catch (error) {
+      openConfirmModal({
+        description: "URL 복사에 실패했습니다.",
+      });
+    }
+  };
   const handleEditStyle = () => {
-    router.push(`/styles/${styleId}/edit`)
-  }
+    router.push(`/styles/${styleId}/edit`);
+  };
 
   const handleDeleteStyle = async (data: StyleDeleteFormInput) => {
     try {
-      await deleteStyle(styleId, data)
+      await deleteStyle(styleId, data);
       openConfirmModal({
-        description: '스타일 삭제가 완료되었습니다. 갤러리 페이지로 이동합니다.',
+        description:
+          "스타일 삭제가 완료되었습니다. 갤러리 페이지로 이동합니다.",
         onClose: () => {
-          router.push('/')
-          router.refresh()
+          router.push("/");
+          router.refresh();
         },
-      })
-      styleDeleteFormModal.closeModal()
+      });
+      styleDeleteFormModal.closeModal();
     } catch (error) {
       openConfirmModal({
-        description: '스타일 삭제에 실패했습니다.',
-      })
+        description: "스타일 삭제에 실패했습니다.",
+      });
     }
-  }
+  };
 
   return (
     <>
       <OptionButtonsLayout
-        onClickEdit={() => { handleEditStyle() }}
-        onClickDelete={() => { styleDeleteFormModal.openModal() }}
+        onClickEdit={() => {
+          handleEditStyle();
+        }}
+        onClickDelete={() => {
+          styleDeleteFormModal.openModal();
+        }}
+        onClickCopy={handleCopyUrl}
       />
       <FormModal
         ref={styleDeleteFormModal.modalRef}
         onClose={styleDeleteFormModal.closeModal}
-        title='삭제 권한 인증'
-        content={(
+        title="삭제 권한 인증"
+        content={
           <StyleDeleteForm
             onSubmit={handleDeleteStyle}
             onClose={styleDeleteFormModal.closeModal}
           />
-        )}
+        }
       />
       {renderConfirmModal()}
     </>
-  )
-}
+  );
+};
 
-export default StyleOptionButtons
+export default StyleOptionButtons;
