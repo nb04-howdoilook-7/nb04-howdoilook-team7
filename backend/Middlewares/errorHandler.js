13fbb17fa72d7f1e5090f16500c8254538b957b1
오연진 <yonjin.oh@gmail.com>
2025-08-26 13:04:42 +0900

    feat: 큐레이션 중복 에러 메시지 수정
    backend/Middlewares/errorHandler.js
    큐레이션 등록한 사용자가 중복 등록 불가하도록 에러 핸들링 추가
    
    frontend/src/services/fetch.ts
    enhancedFetch를 수정해서 response.ok가 false일 때 백엔드의 JSON 응답에서 실제 오류 메시지를 추출하도록 수정
    
    frontend/src/libs/curating/feature-curating/CuratingCreateButton.tsx
    handleCreateCurating의 catch 블록을 수정해서 error.message가 "이미 큐레이션을 등록하셨습니다."이면 해당 메시지를 표시하도록 함. else로 "큐레이팅 등록에 실패했습니다."를 표시
    
    related to: #78

--- backend/Middlewares/errorHandler.js
+++ backend/Middlewares/errorHandler.js
@@ -1,6 +1,8 @@
 export default function errorHandler(err, req, res, next) {
   console.error(err);
-  if (err?.code === 'P2025') {
+  if (err.message && err.message.includes('해당 스타일에 이미 큐레이션을 등록한 사용자입니다.')) {
+    res.status(400).json({ error: '이미 큐레이션을 등록하셨습니다.' });
+  } else if (err?.code === 'P2025') {
     res.status(404).json({ error: 'id를 찾을 수 없습니다.' });
   } else if (err?.name === 'StructError') {
     res.status(400).json({
@@ -10,6 +12,6 @@ export default function errorHandler(err, req, res, next) {
   } else if (err?.statusCode) {
     res.status(err.statusCode).json({ error: err.message });
   } else {
-    res.status(500).json({ error: 'server error' });
+    res.status(500).json({ error: '큐레이션 생성에 실패했습니다.' });
   }
 }
