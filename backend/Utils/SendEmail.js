c3c5e3ef7c8845b6d37e84d858d8683e4cdc3db1
aprkal12 <ehdehd0175@gmail.com>
2025-09-25 14:50:43 +0900

    feat: 이메일 전송 방식 변경

--- backend/Utils/SendEmail.js
+++ backend/Utils/SendEmail.js
@@ -1,26 +1,30 @@
 import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: process.env.EMAIL_PORT,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 
 async function sendEmail(to, subject, text) {
   const mailOptions = {
-    from: process.env.EMAIL_USER,
+    from: process.env.SENDER_EMAIL,
     to,
     subject,
     text,
   };
 
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent: ' + info.response);
+    // const info = await transporter.sendMail(mailOptions);
+    const info = await sgMail.send(mailOptions);
+    console.log('Email sent: ' + info[0]);
     return info;
   } catch (error) {
     console.error('Error sending email: ', error);
